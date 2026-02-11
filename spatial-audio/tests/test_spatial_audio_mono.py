594b8c30656efcf4bbee85efff674613b93ddf7d
Noam Shabtai <shabtai.noam@gmail.com>
2026-02-11 07:23:31 +0000

    kwargs deep copy in a start of a test.

--- spatial-audio/tests/test_spatial_audio_mono.py
+++ spatial-audio/tests/test_spatial_audio_mono.py
@@ -1,8 +1,10 @@
+import copy
+
 import spatial_audio.spatial_audio
 
 
 def test_mono_mode_exists(kwargs_mono, project_dir):
-    kwargs = kwargs_mono
+    kwargs = copy.deepcopy(kwargs_mono)
     kwargs["spatial_audio"]["hrtf"]["path"] = project_dir / kwargs["spatial_audio"]["hrtf"]["path"]
     kwargs["spatial_audio"]["initial_azimuth"] = kwargs["test"]["input"]["azimuth"]
     kwargs["spatial_audio"]["initial_elevation"] = kwargs["test"]["input"]["elevation"]
--- system/tests/test_system.py
+++ system/tests/test_system.py
@@ -1,10 +1,12 @@
+import copy
+
 import numpy as np
 
 import system.instances.system
 
 
 def test_system(kwargs_system):
-    kwargs = kwargs_system
+    kwargs = copy.deepcopy(kwargs_system)
     tested = system.instances.system.System(**kwargs)
     size = tested.input_buffer.step_shape
     chunk = np.random.normal(loc=10, scale=10, size=size).astype(kwargs["input_buffer"]["dtype"])
