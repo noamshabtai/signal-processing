ef8690bb8fb0222eda1f2f1884670dd2698bdcc6
Noam Shabtai <shabtai.noam@gmail.com>
2026-02-24 14:08:37 +0200

    refactor system test config yaml files.

--- system/tests/test_system.py
+++ system/tests/test_system.py
@@ -7,9 +7,9 @@ import system.instances.system
 
 def test_system(kwargs_system):
     kwargs = copy.deepcopy(kwargs_system)
-    tested = system.instances.system.System(**kwargs)
+    tested = system.instances.system.System(**kwargs["system"])
     size = tested.input_buffer.step_shape
-    chunk = np.random.normal(loc=10, scale=10, size=size).astype(kwargs["input_buffer"]["dtype"])
+    chunk = np.random.normal(loc=10, scale=10, size=size).astype(kwargs["system"]["input_buffer"]["dtype"])
     while not tested.input_buffer.full:
         tested.execute(chunk)
     assert np.all(tested.outputs["reflector2"] == chunk)
