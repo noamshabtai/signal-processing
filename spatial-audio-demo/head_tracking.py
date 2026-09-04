spatial audio rotation bug fixed. head tracking added to demo.

spatial-audio-demo/head_tracking.py
@@ -11,6 +11,13 @@ MODEL_URL = (
     "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task"
 )
 STOP_TIMEOUT = 2.0
+METRIC_3D_TO_NED = np.array(
+    [
+        [0, 0, 1],
+        [-1, 0, 0],
+        [0, -1, 0],
+    ]
+)
 
 
 def cameras():
@@ -26,7 +33,7 @@ def fetch_model(path):
 
 
 def yaw_pitch_roll(transformation):
-    rotation = np.asarray(transformation)[:3, :3]
+    rotation = METRIC_3D_TO_NED @ np.asarray(transformation)[:3, :3] @ METRIC_3D_TO_NED.T
     yaw = np.rad2deg(np.arctan2(rotation[1, 0], rotation[0, 0]))
     pitch = np.rad2deg(np.arctan2(-rotation[2, 0], np.hypot(rotation[2, 1], rotation[2, 2])))
     roll = np.rad2deg(np.arctan2(rotation[2, 1], rotation[2, 2]))

spatial-audio/src/spatial_audio/spatial_audio.py
@@ -63,7 +63,7 @@ class SpatialAudio:
         Qx = quaternion.from_rotation_vector(self.xaxis * np.deg2rad(roll))
         Qy = quaternion.from_rotation_vector(self.yaxis * np.deg2rad(pitch))
         Qz = quaternion.from_rotation_vector(self.zaxis * np.deg2rad(yaw))
-        self.global_orientation = Qx * Qy * Qz
+        self.global_orientation = Qz * Qy * Qx
 
     def set_head_orientation(self, yaw, pitch, roll):
         self.yaw = yaw
@@ -72,7 +72,7 @@ class SpatialAudio:
         Qx = quaternion.from_rotation_vector(self.xaxis * np.deg2rad(roll))
         Qy = quaternion.from_rotation_vector(self.yaxis * np.deg2rad(pitch))
         Qz = quaternion.from_rotation_vector(self.zaxis * np.deg2rad(yaw))
-        self.head_orientation = self.global_orientation.conjugate() * Qx * Qy * Qz
+        self.head_orientation = self.global_orientation.conjugate() * Qz * Qy * Qx
 
     def combine_head_orientation(self):
         x_CH, y_CH, z_CH = coordinates.coordinates.spherical_to_ned(